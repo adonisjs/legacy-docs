aeb46b1b3548b74cb4d2e7d408bd8f5fdacf3776
Romain Lanz <romain.lanz@slynova.ch>
2017-08-11 14:28:24 +0200

    chore: move docs to root of the repository (#135)

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const path = require('path')
-const { Ignitor } = require('@adonisjs/ignitor')
-
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(path.join(__dirname, './'))
-  .fireHttpServer()
-  .catch(console.error)
